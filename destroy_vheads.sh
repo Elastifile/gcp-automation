7ffc7d6367d15a16d8f9075aa0f23a90b789cb34
Zohar Lev-Shani <zoharlevshani@google.com>
2021-05-31 16:29:08 +0300

    add grafana to destroy vms list

--- destroy_vheads.sh
+++ destroy_vheads.sh
@@ -32,21 +32,19 @@ while getopts "h?:c:a:b:p:" opt; do
 done
 
 # find the host project
-HOSTPROJECT=`gcloud compute instances describe --zone=$EMS_ZONE $CLUSTER_NAME | grep subnetwork | awk -F/ '{print $7}'`
+HOSTPROJECT=`gcloud compute instances describe --zone=$EMS_ZONE --project $PROJECT $CLUSTER_NAME | grep subnetwork | awk -F/ '{print $7}'`
 
-# delete the vheads
-# remove ems delete protection
-gcloud compute instances update $CLUSTER_NAME --zone=$EMS_ZONE --project $PROJECT --no-deletion-protection --quiet &
-sleep 5
 VHEAD_NAME="$CLUSTER_NAME-elfs"
 RA_NAME="$CLUSTER_NAME-ra"
+GRAFANA_NAME="$CLUSTER_NAME-grafana"
 EMS_NAME="$CLUSTER_NAME"
 
 for zone in ${AVAILABILITY_ZONES//,/ }; do
   VMLIST=`gcloud compute instances list --project $PROJECT --filter="name ~ ${VHEAD_NAME}* AND zone:$zone" | grep $VHEAD_NAME | cut -d " " -f 1`
   RALIST=`gcloud compute instances list --project $PROJECT --filter="name ~ ${RA_NAME}* AND zone:$zone" | grep $RA_NAME | cut -d " " -f 1`
-  EMSLIST=`gcloud compute instances list --filter="name:$EMS_NAME AND ZONE:$zone" | grep -E "\b$EMS_NAME(\s|$)" | cut -d " " -f 1`
-  for i in $EMSLIST $VMLIST $RALIST; do
+  GRAFANALIST=`gcloud compute instances list --project $PROJECT --filter="name ~ ${GRAFANA_NAME}* AND zone:$zone" | grep $GRAFANA_NAME | cut -d " " -f 1`
+  EMSLIST=`gcloud compute instances list --project $PROJECT  --filter="name:$EMS_NAME AND ZONE:$zone" | grep -E "\b$EMS_NAME(\s|$)" | cut -d " " -f 1`
+  for i in $EMSLIST $VMLIST $RALIST $GRAFANALIST; do
     (gcloud compute instances update $i --project $PROJECT --zone=$zone --no-deletion-protection --quiet; gcloud compute instances delete $i --project $PROJECT --zone=$zone --quiet) &
   done
 done
